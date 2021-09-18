0767846a282be9564d121eebd89f2841797728a0
Isaac Morrow <isaacmorrowofficial@gmail.com>
2021-09-18 16:17:50 -0700

    Added views for signup, login, and logout

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout
+
+
+def signup_view(request):
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST or None)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/network/')
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'users/signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        # If the form is valid, send people to their network.
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/network/')
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'users/login.html', {'form': form})
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('/')
