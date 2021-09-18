67798fb1e280c57ba06ae71ffbc7b03b62972c06
Isaac Morrow <isaacmorrowofficial@gmail.com>
2021-09-18 16:15:28 -0700

    Added crispy forms and templates to settings

--- src/health/settings.py
+++ src/health/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dashboard',
     'users',
-    'homepage'
+    'homepage',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'health.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
