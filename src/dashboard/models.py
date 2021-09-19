319107e8ec35824f6d1483c73b43b6ae06e67465
Isaac Morrow <isaacmorrowofficial@gmail.com>
2021-09-18 17:13:50 -0700

    Changed the UserData model to ForeignKey

--- src/dashboard/models.py
+++ src/dashboard/models.py
@@ -10,10 +10,10 @@ class UserData(models.Model):
     class Meta:
         verbose_name = 'User Data'    
 
-    user: User = models.OneToOneField(User, on_delete=models.CASCADE)
-    name: str = models.CharField(max_length=30)
-    eye_breaks: int = models.IntegerField()
-    total_break_time: int = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=30)
+    eye_breaks = models.IntegerField()
+    total_break_time = models.IntegerField()
 
     def __str__(self):
         return f"{self.name} | Eye breaks: {self.eye_breaks} | Break time: {self.total_break_time}"
