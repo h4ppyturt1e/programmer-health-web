124f583388fceb1ded9a3d743d8067705b505a49
Isaac Morrow <isaacmorrowofficial@gmail.com>
2021-09-18 23:33:24 -0700

    Updated UserData model

--- src/dashboard/models.py
+++ src/dashboard/models.py
@@ -12,8 +12,11 @@ class UserData(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=30)
-    eye_breaks = models.IntegerField()
-    total_break_time = models.IntegerField()
+    eye_breaks = models.IntegerField(null=True)
+    total_break_time = models.IntegerField(null=True)
+    liters_drunk = models.IntegerField(null=True)
+    minutes_walked = models.IntegerField(null=True)
+
 
     def __str__(self):
         return f"{self.name} | Eye breaks: {self.eye_breaks} | Break time: {self.total_break_time}"
